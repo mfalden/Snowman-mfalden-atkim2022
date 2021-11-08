3bb81f3ea34a89febd0b66e43302be782064a57d
mfalden <malden2023@chadwickschool.org>
2021-11-08 10:08:42 -0800

    built template

---
+++ WordGuesser/src/WordGame/WordGame.cs
@@ -0,0 +1,52 @@
+namespace WordGuesser
+{
+
+    public class WordGame : IWordGame
+    {
+        public string CheckGuess(string guess)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int CountLetter(char guess)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetFullWord()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetGuessedLetters()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetGuessLimit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetIncorrectGuesses()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetWord()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsGameOver()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsGameWon()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
