d1f2b95d2bc4d66a396867361f3327316eb52bc7
mfalden <malden2023@chadwickschool.org>
2021-11-08 10:15:16 -0800

    figured out who gets who

--- WordGuesser/src/WordGame/WordGame.cs
+++ WordGuesser/src/WordGame/WordGame.cs
@@ -3,46 +3,48 @@ namespace WordGuesser
 
     public class WordGame : IWordGame
     {
+        // marielle
         public string CheckGuess(string guess)
         {
             throw new System.NotImplementedException();
         }
-
+// austin 
         public int CountLetter(char guess)
         {
             throw new System.NotImplementedException();
         }
-
+// austin 
         public string GetFullWord()
         {
             throw new System.NotImplementedException();
         }
-
+// marielle
         public string GetGuessedLetters()
         {
             throw new System.NotImplementedException();
         }
-
+//austin 
         public int GetGuessLimit()
         {
             throw new System.NotImplementedException();
         }
-
+// marielle
         public int GetIncorrectGuesses()
         {
             throw new System.NotImplementedException();
         }
-
+// austin 
         public string GetWord()
         {
             throw new System.NotImplementedException();
         }
-
+// marielle
         public bool IsGameOver()
         {
             throw new System.NotImplementedException();
         }
 
+        // austin
         public bool IsGameWon()
         {
             throw new System.NotImplementedException();
