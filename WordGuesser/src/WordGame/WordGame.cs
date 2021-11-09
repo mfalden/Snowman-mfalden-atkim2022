a47558edaaee8f72abb3d9fe809ca36d7af5df56
atkim2022 <atkim2022@chadwickschool.org>
2021-11-09 10:17:19 -0800

    in class work to begin milestone 2

--- WordGuesser/src/WordGame/WordGame.cs
+++ WordGuesser/src/WordGame/WordGame.cs
@@ -1,10 +1,40 @@
 namespace WordGuesser
 {
+    using System;
+    using System.Collections.Generic;
 
     public class WordGame : IWordGame
     {
-        // marielle
-        public string CheckGuess(string guess)
+// marielle
+
+        private string fullWord;
+        private List<char> lettersGuessed;
+        private int incorrectGuesses;
+        private int guessLimit;
+
+        public WordGame(string wordToGuess, int guessLimit)
+        {
+             if (guessLimit <= 0)
+            {
+                throw new ArgumentException($"Illegal guessLimit '{guessLimit}'. The guess limit must be greater than 0.");
+            }
+
+            foreach (char c in wordToGuess)
+            {
+                if (char.IsLetter(c) == false)
+                {
+                    throw new ArgumentException($"Illegal word '{wordToGuess}'. The word must contain only letters.");
+                }
+            }
+
+            // TODO: Initialize member variables
+            this.fullWord = wordToGuess;
+            this.guessLimit = guessLimit;
+            this.incorrectGuesses = 0;
+            this.lettersGuessed = new List<char>();
+        }
+
+            public string CheckGuess(string guess)
         {
             throw new System.NotImplementedException();
         }
