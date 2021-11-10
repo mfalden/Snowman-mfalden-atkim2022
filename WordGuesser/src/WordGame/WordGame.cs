198b11937108340c4937ed8c9a658a4727f2fca4
mfalden <malden2023@chadwickschool.org>
2021-11-10 12:00:30 -0800

    some work done

--- WordGuesser/src/WordGame/WordGame.cs
+++ WordGuesser/src/WordGame/WordGame.cs
@@ -1,13 +1,57 @@
 namespace WordGuesser
 {
+    using System.Collections.Generic;
+
 
     public class WordGame : IWordGame
     {
-        // marielle
+        int guess;
+        int IncorrectGuesses;
+        int guessLimit;
+        private List<int> lettersGuessed;
+        private string wordToGuess;
+
+        // marielle please help me
+        /// <summary>
+        /// Checks a users guess and returns a string message informing them of the result and updating the game state as necessary.
+        /// </summary>
+        /// <param name="guess">The player's guess.</param>
+        /// <returns>The number of times the player's guess appears in the word, if it does.</returns>
         public string CheckGuess(string guess)
         {
-            throw new System.NotImplementedException();
-        }
+            guess = guess.Trim().ToUpper();
+            if (guess.Length != 1)
+            {
+                return "You must guess a single letter.";
+            }
+            else if (char.IsLetter(guess[0]) == false)
+            {
+                return "You can only guess letters.";
+            }
+            else if (this.lettersGuessed.Contains(guess[0]))
+            {
+                return $"You've already guessed {guess}.";
+            }
+            else if (this.wordToGuess.Contains(guess) == false)
+            {
+                this.IncorrectGuesses++;
+                this.lettersGuessed.Add(guess[0]);
+                return $"Ouch! No {guess}s";
+            }
+            else
+            {
+                int count;
+                count = this.CountLetter(guess[0]);
+                if (count == 1)
+                {
+                    return $"There is 1 {guess}";
+                }
+                else
+                {
+                    return $"There are {count} {guess}s";
+                }
+            }
+        }  
 // austin 
         public int CountLetter(char guess)
         {
@@ -31,7 +75,7 @@ namespace WordGuesser
 // marielle
         public int GetIncorrectGuesses()
         {
-            throw new System.NotImplementedException();
+            return this.IncorrectGuesses;
         }
 // austin 
         public string GetWord()
@@ -39,9 +83,13 @@ namespace WordGuesser
             throw new System.NotImplementedException();
         }
 // marielle
+/// <summary>
+/// waaaa
+/// </summary>
+/// <returns></returns>
         public bool IsGameOver()
         {
-            throw new System.NotImplementedException();
+            return this.IncorrectGuesses >= this.guessLimit;
         }
 
         // austin
